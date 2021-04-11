9814ba67562a6859994b71f0edf5c06d8388ec53
Florian <herzog-florian@gmx.de>
2021-04-11 21:39:17 +0200

    Add movement direction

--- main.py
+++ main.py
@@ -3,26 +3,21 @@ def on_button_pressed_a():
     led.toggle(0, 0)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_microbit_id_io_p0_evt():
-    global X_ABSOLUTE
-    X_ABSOLUTE += 1
-    led.toggle(4, 0)
-control.on_event(EventBusSource.MICROBIT_ID_IO_P0,
-    EventBusValue.MICROBIT_EVT_ANY,
-    on_microbit_id_io_p0_evt)
-
 def on_button_pressed_b():
     global X_ABSOLUTE
     X_ABSOLUTE = 0
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def on_pulsed_p0_high():
-    pass
-pins.on_pulsed(DigitalPin.P0, PulseValue.HIGH, on_pulsed_p0_high)
+def on_microbit_id_io_p1_evt():
+    global X_ABSOLUTE
+    X_ABSOLUTE += 1
+    led.toggle(4, 0)
+control.on_event(EventBusSource.MICROBIT_ID_IO_P1,
+    EventBusValue.MICROBIT_EVT_ANY,
+    on_microbit_id_io_p1_evt)
 
 X_ABSOLUTE = 0
-pins.set_events(DigitalPin.P0, PinEventType.EDGE)
-X_ABSOLUTE = 0
+pins.set_events(DigitalPin.P1, PinEventType.EDGE)
 
 def on_forever():
     pass
--- main.ts
+++ main.ts
@@ -1,24 +1,51 @@
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P8, EventBusValue.MICROBIT_PIN_EVT_RISE, function () {
+    if (pins.digitalReadPin(DigitalPin.P12) == 1) {
+        Y_ABSOLUTE += 1
+    } else {
+        Y_ABSOLUTE += -1
+    }
+    led.toggle(2, 0)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_PIN_EVT_RISE, function () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        X_ABSOLUTE += 1
+    } else {
+        X_ABSOLUTE += -1
+    }
+    led.toggle(0, 2)
+})
 input.onButtonPressed(Button.A, function () {
+    basic.showString("X")
     basic.showNumber(X_ABSOLUTE)
+    basic.showString("Y")
+    basic.showNumber(Y_ABSOLUTE)
     led.toggle(0, 0)
 })
-control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_EVT_ANY, function () {
-	
-})
-pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
-    X_ABSOLUTE += 1
-    led.toggle(4, 0)
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P8, EventBusValue.MICROBIT_PIN_EVT_FALL, function () {
+    if (pins.digitalReadPin(DigitalPin.P12) == 0) {
+        Y_ABSOLUTE += 1
+    } else {
+        Y_ABSOLUTE += -1
+    }
+    led.toggle(2, 0)
 })
 input.onButtonPressed(Button.B, function () {
     X_ABSOLUTE = 0
+    Y_ABSOLUTE = 0
 })
-pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
-    X_ABSOLUTE += 1
-    led.toggle(4, 0)
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P0, EventBusValue.MICROBIT_PIN_EVT_FALL, function () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
+        X_ABSOLUTE += 1
+    } else {
+        X_ABSOLUTE += -1
+    }
+    led.toggle(0, 2)
 })
+let Y_ABSOLUTE = 0
 let X_ABSOLUTE = 0
+pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 pins.setEvents(DigitalPin.P0, PinEventType.Edge)
+pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
+pins.setEvents(DigitalPin.P8, PinEventType.Edge)
 X_ABSOLUTE = 0
-basic.forever(function () {
-	
-})
+Y_ABSOLUTE = 0
